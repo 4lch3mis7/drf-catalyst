20b4f136c38623cf00542fed71046ba210587084
4lch3mis7 <4lch3mis7@proton.me>
2025-03-22 15:07:06 +0545

    chore: automate superuser creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ source .venv/bin/activate
 
 python3 manage.py migrate
 python3 manage.py collectstatic --no-input
+python3 manage.py createsuperuser --no-input
 python3 -m celery -A drf_catalyst worker -l info -B -D
 
 exec "$@"
